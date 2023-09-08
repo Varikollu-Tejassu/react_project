f3d045e1429d63da19fe4642a1d140e7e07af04e
Karicharla Harinadh <karicharlaharinadh@jmangroup.com>
2023-09-08 18:49:15 +0530

    Hupdate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+// src/App.js
+
+// Importing required components
+import React from 'react';
+import Signup from './components/signin-up/Signup';
+import Login from './components/signin-up/login';
+// import User_profile from './components/';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      {/* Routing */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+         {/* <Route path="/profile" element={<User_profile />} />  */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+
---
+++ frontend/src/components/signin-up/login.jsx
@@ -0,0 +1,97 @@
+// Importing required components
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import logo from "../../assets/images/login_bg.png"
+// Creating login component
+const Login = () => {
+
+  // Usesate hooks which initializs state value as an empty string
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  return (
+    <div  className ="login-container" >
+    <div className='login-image-container'> 
+        <img src={logo} />
+    </div>
+    <div className="login-form">
+      <form className='login-form-container'>
+         <h2>Sign In</h2>
+        <input type="email" className= "input-field" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" className= "input-field" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required /><br/>
+        <button className='logbtn' class="login-btn" type="submit">Login</button>
+        <p>Don't have account <Link to="/signup">Register here</ Link></p>
+      </form>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   // HandleLogin function which store user login credentials and calls the backend to check wheather they are registered users or not and based respponse
+//   // From server if the user exists then it navigates to user profile page and displays their data 
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+
+//       // Calling backend server here
+//       const response = await fetch('http://localhost:3000/login', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+
+//         //passing login credentials to server for validation
+//         body: JSON.stringify({ email, password }),
+//       });
+
+//       // If response ok (user registered or exist) navigates to profile page
+//       if (response.ok) {
+//         const userData = await response.json();
+//         navigate('/profile', { state: { user: userData } });
+
+//       } else {
+//         const errorData = await response.json();
+//         console.error('Error logging in:', errorData.error);
+//       }
+
+//     }
+
+//     // Catching occuring error
+//     catch (error) {
+//       console.error('Error logging in:', error);
+//     }
+//   };
