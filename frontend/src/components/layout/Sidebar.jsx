d9dac692e735b31e086241d3cf68ff9b45490440
HimanshuSoni <hsonih2000@gmail.com>
2023-09-10 18:33:36 +0530

    18:33

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -2,7 +2,7 @@
 
 /* React Imports - Start */
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../App.css";
 import logo from "../../assets/images/jmanLogo.png";
@@ -14,10 +14,46 @@ import logo from "../../assets/images/jmanLogo.png";
 /* Function - Start */
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   /* Render View Return - Start */
   return (
     <div>
-      <div className="sidebar">
+      <div className={`sidebar ${showSidebar ? "active" : ""}`}>
+        <div className="menu-button" onClick={toggleSidebar}>
+          {showSidebar ? (
+            <div className="sidebar-close-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-x-lg"
+                viewBox="0 0 16 16"
+              >
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+              </svg>
+            </div>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-list"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          )}
+        </div>
         <div className="sidebar-content">
           <ul className="sidebar-menu-top">
             <li>
@@ -36,7 +72,7 @@ const Sidebar = () => {
                 style={{ color: "white", textDecoration: "none" }}
                 to="/dashboard/resourceallocation"
               >
-                Resource Allocation
+                Project Allocation
               </Link>
             </li>
             {/* {userdata && userdata.userType === "admin" ? ( */}
@@ -102,4 +138,3 @@ const Sidebar = () => {
 
 /* Export default functionName */
 export default Sidebar;
-  
